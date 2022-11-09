011bc111a1e84999051ab7400d891c1353378037
abdulrahmanHatem911 <habdelrahman911@gmail.com>
2022-11-09 14:26:41 +0200

    update 1.0.3

--- lib/core/network/remote/dio_helper.dart
+++ lib/core/network/remote/dio_helper.dart
@@ -1,12 +1,6 @@
 import 'package:book/core/network/remote/api_constant.dart';
 import 'package:dio/dio.dart';
 
-//for get data
-//https://api.nytimes.com
-///svc/books/v3/lists/2022-01-20/hardcover-fiction.json?
-///api-key=AW2W9T2v18ccDGzd7Z4iShihGfxAiR02
-
-//https://api.nytimes.com/svc/books/v3/lists/2022-01-20/hardcover-fiction.json?api-key=YOUR_API_KEY
 class DioHelper {
   static Dio? dio;
   static init() {
